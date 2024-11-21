f9e18782ab59c74b4e58d49c0a605ac62496683b
Johnson He <hejohns@umich.edu>
2024-11-21 22:50:13 +0000

    should be no need/want to run the expect script directly

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 export PATH=$(dirname "${BASH_SOURCE[0]}"):$PATH
 #mogor --19 --time 10 --nbThreads 8 --pondering 0
 # use `expect` to pass mogo the rest of the settings
-$(dirname "${BASH_SOURCE[0]}")/run.exp
+expect $(dirname "${BASH_SOURCE[0]}")/run.exp
